8c313247e49ab12a4543f382bba649733af12c4b
Ratila <32056559+Ratila67@users.noreply.github.com>
2025-02-10 14:28:15 +0000

    install yfinance

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import yfinance as yf
+import streamlit as st
+from PIL import Image
+from urlllib.request import urlopen
 
 st.title("Outil d'Analyse de portfeuille d'investissement")
 st.write(
