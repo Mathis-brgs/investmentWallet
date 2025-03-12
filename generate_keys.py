ca17cca66455c2079d1ace0b5201fcac7d780cf6
Ratila <32056559+Ratila67@users.noreply.github.com>
2025-03-12 17:07:12 +0000

    add pages & menu

--- generate_keys.py
+++ generate_keys.py
@@ -7,8 +7,8 @@ names = ["Jerome Weibel", "Mathis Borges"]
 usernames = ["jweibel", "mborges"]
 passwords = ["abc123", "def456"]
 
-hashed_passwords = stauth.Hasher(passwords).generate()
+hashed_passwords = [stauth.Hasher([]).hash(pw) for pw in passwords]  # Hash chaque mot de passe individuellement
 
-file_path = Path(__file__).parent / "hashed_pw.pk1"
+file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,129 +6,32 @@ import pandas as pd
 import requests
 from datetime import datetime, timedelta
 
+dashboard_page = st.Page(
+    page="views/dashboard.py",
+    title="Dashboard",
+    icon="🏠",
+    default=True,
+)
+
+market_page = st.Page(
+    page="views/market.py",
+    title="Market",
+    icon="🏠",
+)
+
+wallet_page = st.Page(
+    page="views/wallet.py",
+    title="Wallet",
+    icon="🏠",
+)
+
+export_page = st.Page(
+    page="views/export.py",
+    title="Export",
+    icon="🏠",
+)
+
+pg = st.navigation(pages=[dashboard_page, market_page, wallet_page, export_page])
+pg.run()
+
 st.title("Outil d'Analyse de Portefeuille d'Investissement")
-st.header("Main Dashboard")
-
-# Définition des crypto
-cryptos = {
-    "Bitcoin": "BTC-USD",
-    "Ethereum": "ETH-USD",
-    "Ripple": "XRP-USD",
-    "Bitcoin Cash": "BCH-USD",
-    "Solana": "SOL-USD"
-}
-
-@st.cache_data
-def get_crypto_history(symbol):
-    """Récupère l'historique des prix d'une crypto en cache"""
-    try:
-        crypto_data = yf.Ticker(symbol)
-        return crypto_data.history(period="max")
-    except Exception as e:
-        st.error(f"Erreur lors de la récupération des données pour {symbol}: {e}")
-        return None
-
-# Récupération des historiques en cache permet de limiter les requetes.
-historical_data = {name: get_crypto_history(symbol) for name, symbol in cryptos.items()}
-
-# Téléchargement des données sur une période donnée
-@st.cache_data
-def download_crypto_data(symbol, start, end):
-    """Télécharge les données d'une crypto pour une période donnée."""
-    try:
-        return yf.download(symbol, start=start, end=end)
-    except Exception as e:
-        st.error(f"Erreur lors du téléchargement des données pour {symbol}: {e}")
-        return None
-
-#Recuperer les tendances du marchés
-@st.cache_data
-def get_top_trending_cryptos():
-    url = "https://api.coingecko.com/api/v3/coins/markets"
-    params = {
-        "vs_currency": "usd",
-        "order": "market_cap_desc",
-        "per_page": 100,
-        "page": 1,
-        "sparkline": False
-    }
-    
-    try:
-        response = requests.get(url, params=params)
-        data = response.json()
-        df = pd.DataFrame(data)
-        
-        # Trier par la plus forte hausse en pourcentage sur 24h
-        df = df.sort_values(by="price_change_percentage_24h", ascending=False)
-        
-        # Sélectionner les 5 cryptos les plus performantes
-        top_5 = df.head(5)[["name", "symbol", "image", "current_price", "price_change_percentage_24h"]]
-        
-        return top_5
-    except Exception as e:
-        st.error(f"Erreur lors de la récupération des tendances : {e}")
-        return pd.DataFrame()
-
-#top5    
-st.header("Top 5 tendances du marchés (24H)")
-top_cryptos = get_top_trending_cryptos()
-
-if not top_cryptos.empty:
-    for index, row in top_cryptos.iterrows():
-        col1, col2, col3, col4 = st.columns([1,2,2,2])
-        with col1:
-            st.image(row["image"], width=40)
-        with col2:
-            st.write(f"**{row['name']} ({row['symbol'].upper()})**")
-        with col3:
-            st.write(f"💲 {row['current_price']}")
-        with col4:
-            st.write(f"📈 {row['price_change_percentage_24h']:.2f} %")
-else:
-    st.write("Impossible de récupérer les tendances du marché pour le moment.")
-
-
-end_date = datetime.today().strftime("%Y-%m-%d")
-start_date = (datetime.today() - timedelta(days=14)).strftime("%Y-%m-%d")
-crypto_data = {name: download_crypto_data(symbol, start_date, end_date) for name, symbol in cryptos.items()}
-
-st.write(f"Période analysée : {start_date} → {end_date}")
-
-titres_onglets = ['Bitcoin', 'Ethereum', 'Solana']
-onglet1, onglet2, onglet3 = st.tabs(titres_onglets)
-
-with onglet1:
-    # Affichage des données Bitcoin
-    st.subheader("Bitcoin ($)")
-    btc_img_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'
-    st.image(Image.open(urlopen(btc_img_url)))
-
-    if crypto_data["Bitcoin"] is not None:
-        st.table(crypto_data["Bitcoin"])
-        st.line_chart(crypto_data["Bitcoin"]["Close"])
-    else:
-        st.warning("Impossible d'afficher les données Bitcoin.")
-
-with onglet2:
-# Affichage des données Ethereum
-    st.subheader("Ethereum ($)")
-    eth_img_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'
-    st.image(Image.open(urlopen(eth_img_url)))
-
-    if crypto_data["Ethereum"] is not None:
-        st.table(crypto_data["Ethereum"])
-        st.line_chart(crypto_data["Ethereum"]["Close"])
-    else:
-        st.warning("Impossible d'afficher les données Ethereum.")
-
-with onglet3:
-# Affichage des données Solana
-    st.subheader("Solana ($)")
-    sol_img_url = 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png'
-    st.image(Image.open(urlopen(sol_img_url)))
-
-    if crypto_data["Solana"] is not None:
-        st.table(crypto_data["Solana"])
-        st.line_chart(crypto_data["Solana"]["Close"])
-    else:
-        st.warning("Impossible d'afficher les données Solana.")
